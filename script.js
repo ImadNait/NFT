6d5b37bc786e804919da7903ff988a4a0008cb15
Imad <153989803+ImadNait@users.noreply.github.com>
2023-12-30 15:03:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let rightSection = document.querySelector("main .container > .r-section");
+let leftSection = document.querySelector("main .container > .l-section");
+let textContent = leftSection.firstElementChild;
+// make the right section appears after the text when the user in mobile
+const afterLeftSection = _ => leftSection.after(rightSection);
+const afterTextContent = __ => textContent.after(rightSection);
+let screenWidth = window.innerWidth;
+if (screenWidth >= 768) {
+    afterLeftSection();
+} else {
+    afterTextContent();
+}
+
+window.onresize = () => {
+    let screenWidth = window.innerWidth;
+    if (screenWidth >= 768) {
+        afterLeftSection();
+    } else {
+        afterTextContent();
+    }
+}
+
